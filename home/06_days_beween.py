59fe344f5f5a568dd24723f488d25bfda2dc3699
Nik D <nvdudinov@gmail.com>
2022-06-07 22:13:41 +0300

    Add home 06, 07

---
+++ home/06_days_beween.py
@@ -0,0 +1,19 @@
+import datetime
+
+
+def days_diff(a: tuple, b: tuple):
+    date1 = datetime.date(year=a[0], month=a[1], day=a[2])
+    date2 = datetime.date(year=b[0], month=b[1], day=b[2])
+    diff = date2 - date1
+    return abs(diff.days)
+
+
+if __name__ == "__main__":
+    print("Example:")
+    print(days_diff((1982, 4, 19), (1982, 4, 22)))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
+    print("Coding complete? Click 'Check' to earn cool rewards!")
